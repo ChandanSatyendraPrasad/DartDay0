7ba6eacb695fb34ff02222d094efca472a0d4adb
91898 <chandansatyendraprasad@gmail.com>
2022-07-01 12:59:52 +0530

    Initial commit

---
+++ main/test.dart
@@ -0,0 +1,9 @@
+void main(){
+  print("Hello World - Double Quotes");
+  print('Hello World - Single Quote"s');
+  print("Hello World - Double Quote's");
+  print('3+5');
+  print('""3+5""');
+  print(7+8);
+  print('Sum of 6 and 9 = ${6+9}');
+}
